80136f8df1c51b64f30895a311e654b3159f0909
gians <giansluca@gmail.com>
2023-04-15 13:17:16 +0200

    first commit

---
+++ src/utils/cryptography.js
@@ -0,0 +1,65 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { secp256k1 } = require("ethereum-cryptography/secp256k1");
+const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
+const { publicKeyConvert } = require("ethereum-cryptography/secp256k1-compat");
+
+function hashSha256(message) {
+    const bytes = utf8ToBytes(message);
+    const hash = sha256(bytes);
+    return hash;
+}
+
+function hashKeccak256(message) {
+    const bytes = utf8ToBytes(message);
+    const hash = keccak256(bytes);
+    return hash;
+}
+
+/**
+ * ECDSA -> secp256k1 curve
+ * Private keys are 32 bytes long --> 64 bytes in hex
+ *
+ * Public keys are 65 bytes (uncompressed form) or 33 bytes (compressed form) long
+ * first byte is a prefix. 130 or 66 bytes in hex
+ */
+function signSecp256k1(hash, PRIVATE_KEY) {
+    const signature = secp256k1.sign(hash, PRIVATE_KEY);
+    return signature;
+}
+
+/**
+ * To extract the address from public key (public key has to be in uncompressed form)
+ * remove the first byte that indicates the format of the key, whether it is in the compressed format or not
+ * next take the keccak hash of the rest of the public key
+ * finally take the last 20 bytes of the keccak hash
+ *
+ * The ethereum address is 20 bytes --> 40 bytes in hex
+ */
+function getAddressFromPublicKey(publicKey) {
+    if (publicKey.length !== 65 && publicKey.length !== 33) throw new Error("Invalid public key");
+
+    if (publicKey.length === 33) {
+        // public key is compressed
+        publicKey = decompressPublicKey(publicKey);
+    }
+
+    const key = publicKey.slice(1);
+    const hashKey = keccak256(key);
+    const address = hashKey.slice(hashKey.length - 20, key.length);
+
+    return toHex(address);
+}
+
+function decompressPublicKey(publicKey) {
+    if (publicKey.length !== 33) throw new Error("Invalid  compressed public key");
+    return publicKeyConvert(publicKey, false);
+}
+
+module.exports = {
+    hashSha256: hashSha256,
+    hashKeccak256: hashKeccak256,
+    signSecp256k1: signSecp256k1,
+    getAddressFromPublicKey: getAddressFromPublicKey,
+    decompressPublicKey: decompressPublicKey,
+};
---
+++ test/utils/cryptography.test.js
@@ -0,0 +1,97 @@
+const { expect } = require("chai");
+const { secp256k1 } = require("ethereum-cryptography/secp256k1");
+const { publicKeyConvert } = require("ethereum-cryptography/secp256k1-compat");
+const { toHex } = require("ethereum-cryptography/utils");
+const {
+    hashSha256,
+    hashKeccak256,
+    signSecp256k1,
+    getAddressFromPublicKey,
+    decompressPublicKey,
+} = require("../../src/utils/cryptography");
+
+describe("Cryptography", function () {
+    describe("Hash Message", function () {
+        it("should hash a message with sha256 algorithm", async function () {
+            // Given
+            const message = "hello world";
+
+            // When
+            const hash = hashSha256(message);
+            console.log(`hex sha256 hash of: '${message}' is: ${toHex(hash)} `);
+
+            // Then
+            expect(hash).to.not.null;
+            expect(hash).to.be.a("Uint8Array");
+            expect(hash).to.have.lengthOf(32);
+        });
+
+        it("should hash a message with keccak256 algorithm", async function () {
+            // Given
+            const message = "hello world";
+
+            // When
+            const hash = hashKeccak256(message);
+
+            console.log(`hex Keccak256 hash of: '${message}' is: ${toHex(hash)} `);
+
+            // Then
+            expect(hash).to.not.null;
+            expect(hash).to.be.a("Uint8Array");
+            expect(hash).to.have.lengthOf(32);
+        });
+    });
+
+    describe("Sign Message", function () {
+        it("should sign a hash with secp256k1 algorithm and recover public key", async function () {
+            // Given
+            const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+            const publicKey = secp256k1.getPublicKey(PRIVATE_KEY);
+            const message = "hello world";
+
+            // When
+            const hash = hashKeccak256(message);
+            const signature = signSecp256k1(hash, PRIVATE_KEY);
+
+            const recoveredPublicKey = signature.recoverPublicKey(hash).toRawBytes();
+            const isSigned = secp256k1.verify(signature, hash, recoveredPublicKey);
+
+            // Then
+            expect(isSigned).to.be.true;
+            expect(recoveredPublicKey).has.length(33);
+            expect(toHex(publicKey)).to.be.equal(toHex(recoveredPublicKey));
+        });
+    });
+
+    describe("Sign Message", function () {
+        it("should extract the Ethereum address from decompressed public key", async function () {
+            // Given
+            const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+            const EXPECTED_ADDRESS = "16bB6031CBF3a12B899aB99D96B64b7bbD719705";
+            const unCompressedPublicKey = secp256k1.getPublicKey(PRIVATE_KEY, false);
+
+            // When
+            const address = getAddressFromPublicKey(unCompressedPublicKey);
+
+            // Then
+            expect(unCompressedPublicKey).has.length(65);
+            expect(address).has.length(40);
+            expect(address.toUpperCase()).to.be.equal(EXPECTED_ADDRESS.toUpperCase());
+        });
+
+        it("should extract the Ethereum address from compressed public key", async function () {
+            // Given
+            const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+            const EXPECTED_ADDRESS = "16bB6031CBF3a12B899aB99D96B64b7bbD719705";
+            const compressedPublicKey = secp256k1.getPublicKey(PRIVATE_KEY);
+
+            // When
+            const address = getAddressFromPublicKey(compressedPublicKey);
+
+            // Then
+            expect(compressedPublicKey).has.length(33);
+            expect(address).has.length(40);
+            expect(address.toUpperCase()).to.be.equal(EXPECTED_ADDRESS.toUpperCase());
+        });
+    });
+});
