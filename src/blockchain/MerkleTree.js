f029e9a56a747677c23cc65790ecd687c2f612fd
gians <giansluca@gmail.com>
2023-05-01 23:35:32 +0200

    update

--- src/blockchain/MerkleTree.js
+++ src/blockchain/MerkleTree.js
@@ -5,7 +5,21 @@ class MerkleTree {
     }
 
     getRoot() {
-        return this.concat(this.leaves[0], this.leaves[1]);
+        return this._getRoot(this.leaves);
+    }
+
+    _getRoot(currentLayer) {
+        if (currentLayer.length == 1) return currentLayer[0];
+
+        const nextLayer = [];
+        for (let i = 0; i < currentLayer.length; i += 2) {
+            if (i + 1 == currentLayer.length) break;
+
+            const newNode = this.concat(currentLayer[i], currentLayer[i + 1]);
+            nextLayer.push(newNode);
+        }
+
+        return this._getRoot(nextLayer);
     }
 }
 
--- test/blockchain/MerkleTree.test.js
+++ test/blockchain/MerkleTree.test.js
@@ -2,6 +2,18 @@ const { expect } = require("chai");
 const MerkleTree = require("../../src/blockchain/MerkleTree");
 
 describe("Merkle Tree", function () {
+    it("should handle the base case: [A]", function () {
+        // Given
+        const leaves = ["A"];
+        const concat = (a, b) => `Hash(${a} + ${b})`;
+
+        // When
+        const merkleTree = new MerkleTree(leaves, concat);
+
+        // Then
+        expect(merkleTree.getRoot()).to.be.equal("A");
+    });
+
     it("should create a root from two leaves: [A,B]", function () {
         // Given
         const leaves = ["A", "B"];
@@ -12,5 +24,31 @@ describe("Merkle Tree", function () {
 
         // Then
         expect(merkleTree.getRoot()).to.be.equal("Hash(A + B)");
+    });
+
+    it("should create a root from four leaves: [A,B,C,D]", function () {
+        // Given
+        const leaves = ["A", "B", "C", "D"];
+        const concat = (a, b) => `Hash(${a} + ${b})`;
+
+        // When
+        const merkleTree = new MerkleTree(leaves, concat);
+
+        // Then
+        expect(merkleTree.getRoot()).to.be.equal("Hash(Hash(A + B) + Hash(C + D))");
+    });
+
+    it("should create a root from eight leaves: [A,B,C,D,E,F,G,H]", function () {
+        // Given
+        const leaves = ["A", "B", "C", "D", "E", "F", "G", "H"];
+        const concat = (a, b) => `Hash(${a} + ${b})`;
+
+        // When
+        const merkleTree = new MerkleTree(leaves, concat);
+
+        // Then
+        expect(merkleTree.getRoot()).to.be.equal(
+            "Hash(Hash(Hash(A + B) + Hash(C + D)) + Hash(Hash(E + F) + Hash(G + H)))"
+        );
     });
 });
