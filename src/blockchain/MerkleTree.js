a79b5424737a440069134e7f67f286a4950b6243
gians <giansluca@gmail.com>
2023-05-05 00:22:25 +0200

    MerkleTree algorithm

--- src/blockchain/MerkleTree.js
+++ src/blockchain/MerkleTree.js
@@ -11,9 +11,41 @@ class MerkleTree {
     _getRoot(currentLayer) {
         if (currentLayer.length == 1) return currentLayer[0];
 
+        const nextLayer = this._getNextLayer(currentLayer);
+        return this._getRoot(nextLayer);
+    }
+
+    getProof(index) {
+        const proof = [];
+        this._getProof(this.leaves, index, proof);
+
+        return proof;
+    }
+
+    _getProof(currentLayer, currentIndex, proof) {
+        if (currentLayer.length == 1) return;
+
+        const currentNodeIsOdd = this._isOddNode(currentLayer, currentIndex);
+        if (!currentNodeIsOdd) {
+            const currentNodeIsLeft = this._isEvenIndex(currentIndex);
+            const proofNode = currentNodeIsLeft ? currentLayer[currentIndex + 1] : currentLayer[currentIndex - 1];
+            const proofNodeIsLeft = !currentNodeIsLeft;
+
+            proof.push({ data: proofNode, left: proofNodeIsLeft });
+        }
+
+        // Since our merkle tree is a binary tree, each layer combines its pairs resulting in half the number of nodes.
+        // This means we should cut our index in half and round down.
+
+        const nextLayer = this._getNextLayer(currentLayer);
+        const nextIndex = Math.floor(currentIndex / 2);
+        this._getProof(nextLayer, nextIndex, proof);
+    }
+
+    _getNextLayer(currentLayer) {
         const nextLayer = [];
         for (let i = 0; i < currentLayer.length; i += 2) {
-            if (i + 1 == currentLayer.length) {
+            if (this._isOddNode(currentLayer, i)) {
                 const oddNode = currentLayer[i];
                 nextLayer.push(oddNode);
                 break;
@@ -23,7 +55,17 @@ class MerkleTree {
             nextLayer.push(newNode);
         }
 
-        return this._getRoot(nextLayer);
+        return nextLayer;
+    }
+
+    _isOddNode(currentLayer, index) {
+        if (index + 1 == currentLayer.length && this._isEvenIndex(index)) return true;
+        else return false;
+    }
+
+    _isEvenIndex(number) {
+        if (number % 2 == 0) return true;
+        else return false;
     }
 }
 
--- test/blockchain/MerkleTree.test.js
+++ test/blockchain/MerkleTree.test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const crypto = require("crypto");
 const MerkleTree = require("../../src/blockchain/MerkleTree");
 
 describe("Merkle Tree", function () {
@@ -87,4 +88,63 @@ describe("Merkle Tree", function () {
         // Then
         expect(merkleTree.getRoot()).to.be.equal("Hash(Hash(Hash(A + B) + Hash(C + D)) + Hash(Hash(E + F) + G))");
     });
+
+    describe("merkle proof", function () {
+        const leaves = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+        const root = "eb100814abc896ab18bcf6c37b6550eeadeae0c312532286a4cf4be132ace526";
+        const hashTree = new MerkleTree(leaves.map(sha256), concatHash);
+        const lettersTree = new MerkleTree(leaves, concatLetters);
+
+        describe("for each leaf", function () {
+            leaves.forEach((leaf, i) => {
+                it(`should return a proof that calculates the root from leaf ${leaves[i]}`, function () {
+                    const proof = hashTree.getProof(i);
+                    const hashedProof = hashProof(leaf, proof).toString("hex");
+
+                    if (hashedProof !== root) {
+                        const lettersProof = lettersTree.getProof(i);
+
+                        console.log(
+                            "The resulting hash of your proof is wrong. \n" +
+                                `We were expecting: ${root} \n` +
+                                `We received: ${hashedProof} \n` +
+                                `In ${leaves.join("")} Merkle tree, the proof of ${leaves[i]} you gave us is: \n` +
+                                `${JSON.stringify(lettersProof, null, 2)}`
+                        );
+                    }
+
+                    expect(hashedProof).to.be.equal(root);
+                });
+            });
+        });
+    });
 });
+
+// use the crypto module to create a sha256 hash from the data passed in
+function sha256(data) {
+    return crypto.createHash("sha256").update(data).digest();
+}
+
+// the concat function we use to hash together merkle leaves
+function concatHash(left, right) {
+    if (!left) throw new Error("The concat function expects two hash arguments, the first was not received.");
+    if (!right) throw new Error("The concat function expects two hash arguments, the second was not received.");
+
+    return sha256(Buffer.concat([left, right]));
+}
+
+// the concat function we use to show the merkle root calculation
+function concatLetters(left, right) {
+    return `Hash(${left} + ${right})`;
+}
+
+// given a proof, finds the merkle root
+function hashProof(node, proof) {
+    let data = sha256(node);
+    for (let i = 0; i < proof.length; i++) {
+        const buffers = proof[i].left ? [proof[i].data, data] : [data, proof[i].data];
+        data = sha256(Buffer.concat(buffers));
+    }
+
+    return data;
+}
