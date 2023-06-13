20ba698dc59dada6bdc572c635bf887dddb936d9
gians <giansluca@gmail.com>
2023-06-14 00:50:00 +0200

    transaction with ethers

--- src/ethers/wallet.js
+++ src/ethers/wallet.js
@@ -1,5 +1,5 @@
 const ethers = require("ethers");
-const { Wallet } = ethers;
+const { Wallet, parseEther } = ethers;
 
 // create a wallet with a private key
 const wallet1 = new Wallet("0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d");
@@ -7,7 +7,14 @@ const wallet1 = new Wallet("0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656
 // create a wallet from mnemonic
 const wallet2 = Wallet.fromPhrase("plate lawn minor crouch bubble evidence palace fringe bamboo laptop dutch ice");
 
+const signaturePromise = wallet1.signTransaction({
+    value: parseEther("1"),
+    to: "0xdD0DC6FB59E100ee4fA9900c2088053bBe14DE92",
+    gasLimit: 21000,
+});
+
 module.exports = {
     wallet1: wallet1,
     wallet2: wallet2,
+    signaturePromise: signaturePromise,
 };
--- test/ethers/wallet.test.js
+++ test/ethers/wallet.test.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
-const { wallet1, wallet2 } = require("../../src/ethers/wallet");
-const { Wallet, HDNodeWallet } = require("ethers");
+const { wallet1, wallet2, signaturePromise } = require("../../src/ethers/wallet");
+const { Wallet, HDNodeWallet, Transaction, encodeBytes32String } = require("ethers");
 
 describe("wallets", () => {
     describe("wallet 1", () => {
@@ -19,6 +19,34 @@ describe("wallets", () => {
 
         it("should unlock the expected address", () => {
             expect(wallet2.address).to.be.equals("0x88E9DD325BA8329dDD9825c1d24e8470b25575C1");
+        });
+    });
+});
+
+describe("signaturePromise", () => {
+    it("should be an instance of Promise", () => {
+        expect(signaturePromise).to.be.instanceof(Promise);
+    });
+
+    it("should resolve with a hexadecimal representation of the transaction", async () => {
+        const hex = await signaturePromise;
+        const matches = /^0x[0-9A-Fa-f]*$/.test(hex);
+        if (!matches) console.log(hex);
+
+        expect(matches).to.be.true;
+    });
+
+    describe("parsed properties", () => {
+        it("should contain the properties", async () => {
+            // given - when
+            const hex = await signaturePromise;
+            const parsed = Transaction.from(hex);
+
+            // then
+            expect(parsed.to).to.be.equals("0xdD0DC6FB59E100ee4fA9900c2088053bBe14DE92");
+            expect(parsed.value.toString()).to.be.equal("1000000000000000000");
+            expect(parsed.gasLimit).to.be.equal(21000n);
+            expect(parsed.from).to.be.equal("0x5409ED021D9299bf6814279A6A1411A7e866A631");
         });
     });
 });
