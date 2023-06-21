0ad2d7c7c8206542c072d38b9b657b41ef8a16d9
gians <giansluca@gmail.com>
2023-06-21 17:07:53 +0200

    update

--- src/ethers/sendEthers.js
+++ src/ethers/sendEthers.js
@@ -5,12 +5,15 @@ async function sendEther({ amount, to }) {
     const provider = new InfuraProvider("sepolia", config.secret.infuraApiKey);
     const signer = new Wallet(config.secret.accountPrivateKey, provider);
 
+    const gasLimit = 21000;
+    const gasPrice = 1000000000;
+
     // sign and send
     const tx = await signer.sendTransaction({
         value: amount,
         to: to,
-        gasLimit: 0x5208,
-        gasPrice: 0x3b9aca00,
+        gasLimit: `0x${gasLimit.toString(16)}`,
+        gasPrice: `0x${gasPrice.toString(16)}`,
     });
 
     return tx;
